5d2787ea286bf38894de89ca147ea22b393b45b0
Arshq <marshq23@gmail.com>
2026-06-24 11:50:52 +0700

    feat: added about section to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/navbar";
 import { HeroSection } from "./sections/HeroSection";
+import { AboutSection } from "./sections/AboutSection";
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <Navbar />
       <main>
         <HeroSection />
+        <AboutSection />
       </main>
     </div>
   );
